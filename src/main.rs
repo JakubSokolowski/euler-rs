558d8c6549cb9cc2fd1f6151b456716391da104b
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-08-02 20:24:37 +0200

    Add problem 27 solution

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ fn main() {
         23 => problems::p23_non_abundant_sums::run(),
         24 => problems::p24_lexicographic_permutations::run(),
         26 => problems::p26_reciprocal_cycles::run(),
+        27 => problems::p27_quadratic_primes::run(),
         75 => problems::p75_singular_integer_right_triangles::run(),
         _ => panic!("Problem num: {} not implemented", problem_num),
     }
---
+++ src/problems/p27_quadratic_primes.rs
@@ -0,0 +1,115 @@
+extern crate test;
+use itertools::Itertools;
+
+pub fn run() {
+    find_max_coefficient_product(999, 1000);
+}
+
+fn find_max_coefficient_product(a_abs_limit: i32, b_abs_limit: i32) -> i32 {
+    println!(
+        "Searching for quadratic primes a: {} b: {}",
+        a_abs_limit, b_abs_limit
+    );
+
+    let mut attempts = 0;
+    let res = (-a_abs_limit..a_abs_limit)
+        .cartesian_product(-b_abs_limit..b_abs_limit)
+        .inspect(|_| attempts += 1)
+        .map(|(a, b)| ((a, b), num_generated_primes(a, b)))
+        .max_by(|x, y| x.1.cmp(&y.1))
+        .unwrap();
+
+    let (a, b) = res.0;
+    let product = a * b;
+
+    println!(
+        "Attempts: {} Max consecutive primes: {:?} Coefficient product: {}",
+        attempts, res, product
+    );
+
+    product
+}
+
+fn is_prime(n: i32) -> bool {
+    let limit = (n as f64).sqrt() as i32;
+
+    for i in 2..=limit {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn num_generated_primes(a: i32, b: i32) -> i32 {
+    let mut n = 0;
+    loop {
+        let maybe_prime = quadratic_formula(a, b, n);
+        if is_prime(maybe_prime.abs()) {
+            n += 1;
+        } else {
+            break;
+        }
+    }
+    n
+}
+
+fn quadratic_formula(a: i32, b: i32, n: i32) -> i32 {
+    n.pow(2) + a * n + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn test_num_generated_primes_for_a79_b1601() {
+        // given
+        let a = -79;
+        let b = 1601;
+
+        // when
+        let result = num_generated_primes(a, b);
+
+        // then
+        let expected = 80;
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_num_generated_primes_for_a1_b41() {
+        // given
+        let a = 1;
+        let b = 41;
+
+        // when
+        let result = num_generated_primes(a, b);
+
+        // then
+        let expected = 40;
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_num_generated_primes_for_a999_b61() {
+        // given
+        let a = -999;
+        let b = 61;
+
+        // when
+        let result = num_generated_primes(a, b);
+
+        // then
+        let expected = 11;
+        assert_eq!(expected, result);
+    }
+
+    #[bench]
+    fn bench_find_coefficient(bencher: &mut Bencher) {
+        let a = 999;
+        let b = 1000;
+        bencher.iter(|| find_max_coefficient_product(a, b));
+    }
+}
