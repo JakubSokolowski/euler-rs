d991a975fc4cb2aa86ded9a4b4fc4e153c253ebf
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2021-08-26 20:24:27 +0200

    Add solution to problem 42

--- src/problems/p22_names_scores.rs
+++ src/problems/p22_names_scores.rs
@@ -1,8 +1,8 @@
-use lazy_static::lazy_static;
-use regex::{Match, Regex};
+use crate::words::alphabet::word_value;
+use crate::words::file;
 
 pub fn run(input: &str) {
-    let mut words = extract_words(input);
+    let mut words = file::extract_words(input);
     words.sort();
 
     let total: usize = words
@@ -12,53 +12,4 @@ pub fn run(input: &str) {
         .sum();
 
     println!("Total sum: {}", total)
-}
-
-pub fn alphabet_position(c: char) -> usize {
-    c.to_ascii_uppercase() as usize - 64
-}
-
-pub fn word_value(word: &str) -> usize {
-    word.chars().map(alphabet_position).sum()
-}
-
-pub fn extract_words(s: &str) -> Vec<String> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r#""(.*?)""#).unwrap();
-    }
-
-    RE.find_iter(s)
-        .map(|word: Match| word.as_str().replace("\"", ""))
-        .collect()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_char_position_returns_proper_position() {
-        // given
-        let letter = 'c';
-
-        // when
-        let res = alphabet_position(letter);
-
-        // then
-        let expected = 3;
-        assert_eq!(res, expected);
-    }
-
-    #[test]
-    fn word_value_returns_proper_value() {
-        // given
-        let word = "COLIN";
-
-        // when
-        let res = word_value(word);
-
-        // then
-        let expected = 53;
-        assert_eq!(res, expected);
-    }
 }
